89c254e25d7b3fffb7f0ade77ff35efb028b0e69
David Kamphausen <david.kamphausen@web.de>
2013-03-07 21:50:30 +0100

    arrow keys

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,5 @@
 function Controller(ops) {
+
 	this.keymap = {
 		104 : {
 			x : -1
@@ -40,12 +41,65 @@ function Controller(ops) {
 		}
 	};
 
+	this.keymap = {
+		38 : {
+			y : -1
+		},
+		40 : {
+			y : 1
+		},
+		39 : {
+			x : 1
+		},
+		37 : {
+			x : -1
+		},
+		72 : {
+			x : -1
+		},
+		76 : {
+			x : 1
+		},
+		74 : {
+			y : 1
+		},
+		75 : {
+			y : -1
+		},
+		89 : {
+			x : -1,
+			y : -1
+		},
+		90 : {
+			x : -1,
+			y : -1
+		},
+		66 : {
+			x : -1,
+			y : 1
+		},
+		78 : {
+			x : 1,
+			y : 1
+		},
+		85 : {
+			x : 1,
+			y : -1
+		},
+		60 : {
+			z : 1
+		},
+		62 : {
+			z : -1
+		}
+	};
+
 	$.extend(this, ops);
 	var self = this;
 
 	this.init = function() {
-		$(window).bind("keypress", function(ev) {
-			console.log("EV", ev);
+		$(window).bind("keydown", function(ev) {
+			console.log("EV", ev, ev.keyCode);
 
 			var by = self.keymap[ev.keyCode];
 			if (by) {
