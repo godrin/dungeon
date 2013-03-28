e3ef7a94ae0717be096155b0a2eea5134736ea1e
David Kamphausen <david.kamphausen@web.de>
2013-03-28 20:40:04 +0100

    bigger

--- js/view/field_view.js
+++ js/view/field_view.js
@@ -1,33 +1,33 @@
 function FieldView(ops) {
-	this.el = "#field";
-	this.cellWidth = 60;
-	$.extend(this, ops);
-	var self = this;
-	var cellViews=[];
+  this.el = "#field";
+  this.cellWidth = 128;
+  $.extend(this, ops);
+  var self = this;
+  var cellViews=[];
 
-	this.init = function() {
-		$(self.el).empty();
-		var html="";
+  this.init = function() {
+    $(self.el).empty();
+    var html="";
 
-		self.model.eachCell(function(pcell) {
-			var cell = new CellView({
-				cell : pcell,
-				pel : self.el,
-				x:pcell.x*self.cellWidth,
-				y:pcell.y*self.cellWidth
-			});
-			
-			var x = cell.cell.x * self.cellWidth, y = cell.cell.y
-					* self.cellWidth;
-			/*cell.el.css({
-				left : x,
-				top : y
-			});*/
-			html+=cell.html();
-			cellViews.push(cell);
-		});
-		$(self.el).html(html);
-		$.map(cellViews,function(cellView) {cellView.update();});
+    self.model.eachCell(function(pcell) {
+      var cell = new CellView({
+	cell : pcell,
+	pel : self.el,
+	x:pcell.x*self.cellWidth,
+	y:pcell.y*self.cellWidth
+      });
 
-	};
+      var x = cell.cell.x * self.cellWidth, y = cell.cell.y
+      * self.cellWidth;
+      /*cell.el.css({
+	left : x,
+	top : y
+	});*/
+	html+=cell.html();
+	cellViews.push(cell);
+    });
+    $(self.el).html(html);
+    $.map(cellViews,function(cellView) {cellView.update();});
+
+  };
 }
